4e19c447e07988fa4f16fcf2a48294641ef61c6b
harasya <harasya.email@gmail.com>
2025-03-22 22:29:36 +0700

    initial commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import { onAuthStateChanged } from 'firebase/auth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: HomeView, meta: { requiresAuth: true } },
     {
