0db793bc2c0fb4082ac50a40aa4639fcc2f97aa7
harasya <harasya.email@gmail.com>
2025-03-22 21:33:05 +0700

    initial commit

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getAuth } from 'firebase/auth'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_FIREBASE_BASE_URL,
@@ -7,4 +8,15 @@ const api = axios.create({
   },
 })
 
+api.interceptors.request.use(async (config) => {
+  const auth = getAuth()
+  const user = auth.currentUser
+
+  if (user) {
+    const token = await user.getIdToken()
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
 export default api
